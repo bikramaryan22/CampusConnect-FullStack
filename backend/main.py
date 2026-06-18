3896e93d78f4c9810e81b0c08cdd2abf19e183da
bikramaryan22 <bikramaryan22@gmail.com>
2026-06-18 12:19:38 +0530

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://campus-connect-full-stack.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
